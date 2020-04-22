55986b70780b887e9551c08373075f2b7a94173f
allinmybed <allinmybed@slamgpuw03.iis.sinica.edu.tw>
2020-04-22 19:57:11 +0800

    GAMgr.h

--- include/GAMgr.h
+++ include/GAMgr.h
@@ -4,7 +4,7 @@
 class GAMgr
 {
 public:
-    GAMgr(int p): population(p){};
+    GAMgr(MovingMgr* m, int p, int n): mgr(m), population(p){}, balls_number(n){};
     virtual ~GAMgr();
     void random_initiailize();
     void selection();
@@ -12,7 +12,10 @@ public:
     void crossover();
     void start();
 private:
+	MovingMgr* mgr;
     int population;
+    int balls_number;
+    int order[population][balls_number];
 };
 
 #endif /* GAMGR_H */
